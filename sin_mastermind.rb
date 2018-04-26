09696eafc5b90771fa5102ba9aa97e78440db134
Samuel Masters <redeyetuning@gmail.com>
2018-04-26 16:23:17 +0400

    Start to add #gen_pegs

--- sin_mastermind.rb
+++ sin_mastermind.rb
@@ -3,6 +3,11 @@ require 'sinatra'
 require 'sinatra' 
 require  'sinatra/reloader' if development?
 
+def gen_pegs
+	squares.map{|x| "<div class ='peg' id = #{x}>#{x}</div>"}
+	<div class = "peg" id = "A1">A1</div>
+end
+
 get '/' do
-	"<h1>Sinatra Mastermind </h1>" 
+	erb :board 
 end
