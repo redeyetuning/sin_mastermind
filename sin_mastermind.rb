a1a20f33096d01bf11b87a7b0ddbfbb983505c6f
Samuel Masters <redeyetuning@gmail.com>
2018-04-29 15:48:54 +0400

    Add basic html form, css, and dropdown menu generator methods

--- sin_mastermind.rb
+++ sin_mastermind.rb
@@ -1,13 +1,21 @@
-require './lib/mastermind.rb'
+#require './lib/mastermind.rb'
 require 'sinatra'
-require 'sinatra' 
-require  'sinatra/reloader' if development?
+#require  'sinatra/reloader' if development?
 
-def gen_pegs
-	squares.map{|x| "<div class ='peg' id = #{x}>#{x}</div>"}
-	<div class = "peg" id = "A1">A1</div>
+def gen_col_options
+	cols = ["Blue", "Green", "Orange", "Purple", "Red", "Yellow"]
+	cols.map{|x| "<option value='#{x}'>#{x}</option>"}.join
 end
 
+def gen_pegs css_class
+	squares = *(1..48)
+	squares.map{|x| "<div class ='#{css_class}' id = #{x}></div>"}.join
+end
+
+
 get '/' do
-	erb :board 
+	guess_pegs = gen_pegs("guess-peg")
+	cor_guess_pegs = gen_pegs("cor-guess-peg")
+	col_options = gen_col_options
+	erb :board, locals: {:guess_pegs => guess_pegs, :cor_guess_pegs => cor_guess_pegs, :col_options => col_options} 
 end
